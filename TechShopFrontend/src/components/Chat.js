6e4fd16bbcdaf5af2fede3a6c8f3678381e9e47a
TendouHakai <thanhconghoahai@gmail.com>
2023-07-12 13:02:12 +0700

    update chatbot because old chatbot is expired

--- TechShopFrontend/src/components/Chat.js
+++ TechShopFrontend/src/components/Chat.js
@@ -2,9 +2,16 @@ import React, {useEffect} from "react";
 
 function Chat(){
     useEffect(()=>{
+        // (function(d, m){
+        //     var kommunicateSettings = 
+        //         {"appId":"3615cbfd13a25790f76b60fb463008d03","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        //     var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        //     s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        //     var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        //     window.kommunicate = m; m._globals = kommunicateSettings;
         (function(d, m){
             var kommunicateSettings = 
-                {"appId":"15a1e918f7eeae0799b497375abcbde76","popupWidget":true,"automaticChatOpenOnNavigation":true};
+                {"appId":"3615cbfd13a25790f76b60fb463008d03","popupWidget":true,"automaticChatOpenOnNavigation":true};
             var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
             s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
             var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
